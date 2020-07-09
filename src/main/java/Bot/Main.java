e3270ab70faaff32aac1399207ab22c39a592f38
CaveDann <41668867+CaveDann@users.noreply.github.com>
2020-07-09 18:39:28 +0100

    Change colour with ?colour <colour>.

--- src/main/java/Bot/Main.java
+++ src/main/java/Bot/Main.java
@@ -9,6 +9,8 @@ import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -26,17 +28,17 @@ public class Main {
 
         // Add a listener which answers with "Pong!" if someone writes "!ping"
         api.addMessageCreateListener(event -> {
-            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+            if (event.getMessage().getContent().equalsIgnoreCase("Ping!")) {
                 event.getChannel().sendMessage("Pong!");
             }
         });
 
         // Add a listener which creates a channel in the Game Rooms category, prepended with the user's name, if someone writes "!gameroom"
         api.addMessageCreateListener(event -> {
-            if (event.getMessage().getContent().equalsIgnoreCase("!gameroom")) {
+            if (event.getMessage().getContent().equalsIgnoreCase("?gameroom")) {
                 if (event.getServer().isPresent()) {
                     Server server = event.getServer().get();
-                    ChannelCategory ccat = server.getChannelCategoriesByName("Game Rooms").get(0);
+                    ChannelCategory ccat = server.getChannelCategoriesByName("Lobby").get(0);
                     String uname = event.getMessageAuthor().getDisplayName();
                     ServerVoiceChannel channel = new ServerVoiceChannelBuilder(server)
                             .setCategory(ccat)
@@ -44,6 +46,7 @@ public class Main {
                             .setUserlimit(10)
                             .create()
                             .join();
+                    event.deleteMessage();
 
                     // Delete the channel if the last user leaves
                     channel.addServerVoiceChannelMemberLeaveListener(event2 -> {
@@ -66,11 +69,39 @@ public class Main {
 
         // Add a listener which changes a user's role, if someone writes "!colour
         api.addMessageCreateListener(event -> {
-            if (event.getMessage().getContent().equalsIgnoreCase("!colour")) {
-                Server server = event.getServer().get();
-                User user = event.getMessageAuthor().asUser().get();
-                Role role = server.getRolesByNameIgnoreCase("DS").get(0);
-                server.addRoleToUser(user, role);
+            //if (event.getMessage().getContent().equalsIgnoreCase("!colour")) {
+            if (event.getMessage().getContent().contains("?colour")) {
+                // You're trying to do some stuff with lists here so that you can match parts to the list,
+                // to figure out which role (if any) need to be removed, and then add the list in the command.
+                // Ideally, the final form of this command would be "!colour <colour-code>", which removes any
+                // previous colour, and adds the desired one.
+                //
+                String[] rmessage = event.getMessage().getContent().split("\\s+");
+                String[] rcolours = {"B", "DS", "S", "DO", "O", "DY", "Y", "DPi", "Pi", "DPu", "Pu", "DB", "Bl", "TB", "DG", "G", "DT", "V", "T"};
+                List<String> rclist = Arrays.asList(rcolours);
+
+                if (rclist.contains(rmessage[1])) {
+
+                    Server server = event.getServer().get();
+                    User user = event.getMessageAuthor().asUser().get();
+                    String uname = event.getMessageAuthor().getDisplayName();
+
+                    for (String i : rcolours) {
+                        Role rolei = server.getRolesByNameIgnoreCase(i).get(0);
+                        server.removeRoleFromUser(user, rolei);
+                    }
+
+                    Role newrole = server.getRolesByNameIgnoreCase(rmessage[1]).get(0);
+                    server.addRoleToUser(user, newrole);
+                    //Role roleB = server.getRolesByNameIgnoreCase("B").get(0);
+                    //server.removeRoleFromUser(user, roleB);
+
+                    event.deleteMessage();
+                    event.getChannel().sendMessage(uname + "'s Roles Changed");
+                } else {
+                    event.getChannel().sendMessage(rmessage[1] + " does not exist");
+                    event.getChannel().sendMessage("Colour roles are B (Black), DS (Dark Salmon), S (Salmon), DO (Dark Orange), O (Orange), DY (Dark Yellow), Y (Yellow), DPi (Dark Pink), Pi (Pink), DPu (Dark Purple), DB (Dark Blue), Bl (Blue), TB (TurtleEgg Blue, DG (Dark Green), G (Green), DT (Dark Turquoise), T (Turquoise), V (Viridian)");
+                }
             }
         });
 
