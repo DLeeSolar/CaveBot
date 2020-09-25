a10163585cb4f5709e15102d88edc927c23d0156
CaveDann <41668867+CaveDann@users.noreply.github.com>
2020-09-25 09:23:49 +0100

    Improve Code Comments Post Command Migration
    A lot of spurious comments have been removed to clean up Main now that
    the commands are in their own classes. Similar has been done on the
    Bot.commands.Colour and Bot.commands.Gameroom files. Comments within
    the files have also been improved where neccessary.

--- src/main/java/Bot/Main.java
+++ src/main/java/Bot/Main.java
@@ -19,90 +19,10 @@ public class Main {
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
-        // Add a listener which answers with "Pong!" if someone writes "!ping"
-        //api.addMessageCreateListener(event -> {
-        //    if (event.getMessage().getContent().equalsIgnoreCase("Ping!")) {
-        //        event.getChannel().sendMessage("Pong!");
-        //    }
-        //});
+        // Add listeners for commands
         api.addMessageCreateListener(new PingPong());
-
-        // Add a listener which creates a channel in the Game Rooms category, prepended with the user's name, if someone writes "!gameroom"
         api.addMessageCreateListener(new Gameroom());
-        /*api.addMessageCreateListener(event -> {
-            if (event.getMessage().getContent().equalsIgnoreCase("?gameroom")) {
-                if (event.getServer().isPresent()) {
-                    Server server = event.getServer().get();
-                    ChannelCategory ccat = server.getChannelCategoriesByName("Lobby").get(0);
-                    String uname = event.getMessageAuthor().getDisplayName();
-                    ServerVoiceChannel channel = new ServerVoiceChannelBuilder(server)
-                            .setCategory(ccat)
-                            .setName(uname + "'s Room")
-                            .setUserlimit(10)
-                            .create()
-                            .join();
-                    event.deleteMessage();
-
-                    // Delete the channel if the last user leaves
-                    channel.addServerVoiceChannelMemberLeaveListener(event2 -> {
-                        if (event2.getChannel().getConnectedUserIds().isEmpty()) {
-                            event2.getChannel().delete();
-                        }
-                    });
-
-                    // Delete the channel if no user joined in the first 30 seconds
-                    api.getThreadPool().getScheduler().schedule(() -> {
-                        if (channel.getConnectedUserIds().isEmpty()) {
-                            channel.delete();
-                        }
-                    }, 30, TimeUnit.SECONDS);
-                } else {
-                    event.getChannel().sendMessage("Server not found!");
-                }
-            }
-        });*/
-
-        // Add a listener which changes a user's role, if someone writes "!colour
         api.addMessageCreateListener(new Colour());
-        /*api.addMessageCreateListener(event -> {
-            String content = event.getMessage().getContent();
-            //if (event.getMessage().getContent().equalsIgnoreCase("!colour")) {
-            //if (event.getMessage().getContent().contains("?colour")) {
-            if (content.startsWith("?colour ")) {
-                // You're trying to do some stuff with lists here so that you can match parts to the list,
-                // to figure out which role (if any) need to be removed, and then add the list in the command.
-                // Ideally, the final form of this command would be "!colour <colour-code>", which removes any
-                // previous colour, and adds the desired one.
-                //
-                //String[] rmessage = event.getMessage().getContent().split("\\s+");
-                String[] rmessage = content.split("\\s+");
-                String[] rcolours = {"B", "DS", "S", "DO", "O", "DY", "Y", "DPi", "Pi", "DPu", "Pu", "DB", "Bl", "TB", "DG", "G", "DT", "V", "T"};
-                List<String> rclist = Arrays.asList(rcolours);
-
-                if (rclist.contains(rmessage[1])) {
-
-                    Server server = event.getServer().get();
-                    User user = event.getMessageAuthor().asUser().get();
-                    String uname = event.getMessageAuthor().getDisplayName();
-
-                    for (String i : rcolours) {
-                        Role rolei = server.getRolesByNameIgnoreCase(i).get(0);
-                        server.removeRoleFromUser(user, rolei);
-                    }
-
-                    Role newrole = server.getRolesByNameIgnoreCase(rmessage[1]).get(0);
-                    server.addRoleToUser(user, newrole);
-                    //Role roleB = server.getRolesByNameIgnoreCase("B").get(0);
-                    //server.removeRoleFromUser(user, roleB);
-
-                    event.deleteMessage();
-                    event.getChannel().sendMessage(uname + "'s Roles Changed");
-                } else {
-                    event.getChannel().sendMessage(rmessage[1] + " does not exist");
-                    event.getChannel().sendMessage("Colour roles are B (Black), DS (Dark Salmon), S (Salmon), DO (Dark Orange), O (Orange), DY (Dark Yellow), Y (Yellow), DPi (Dark Pink), Pi (Pink), DPu (Dark Purple), DB (Dark Blue), Bl (Blue), TB (TurtleEgg Blue, DG (Dark Green), G (Green), DT (Dark Turquoise), T (Turquoise), V (Viridian)");
-                }
-            }
-        });*/
 
         // Print the invite url of your bot
         System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
